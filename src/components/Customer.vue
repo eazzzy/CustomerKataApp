0afb9a2dd603e801d1e50938c3e909340d3aef4b
Nico <nico@kowhiri.com>
2019-06-16 23:45:00 +1200

    Get data from customer.api

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -3,3 +3,22 @@
         <h1>Customers</h1>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      apiData: []
+    }
+  },
+  mounted: function() {
+    fetch('http://localhost:51770/api/customer', { 
+      method: 'get' 
+    })
+    .then((response) => response.json())
+    .then((jsonData) => {
+      this.apiData = jsonData
+    })
+  }
+}
+</script>
