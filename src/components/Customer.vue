9bef489c83bb7cd76fac5183addc72df0017ff4b
Nico <nico@kowhiri.com>
2019-06-17 01:15:24 +1200

    Changed to filter from API

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -3,14 +3,14 @@
         <h4>Customers</h4>
          <div class="form-row">
           <div class="col-10">
-            <input type="text" class="form-control" placeholder="Enter key word  ..." v-model="search" v-on:keyup="getfilteredData">
+            <input type="text" class="form-control" placeholder="Enter key word  ..." v-model="search" v-on:keyup="getDataFromApi">
           </div>
         </div>
         <p class="mt-3" align="right">Page: {{ currentPage }} of {{ Math.ceil(rows/perPage) }}</p>
        <div>
           <b-table id="cust-table"
             striped
-            :items="filteredData"
+            :items="apiData"
             :per-page="perPage"
             :current-page="currentPage">
           </b-table>
@@ -44,28 +44,33 @@ export default {
   },
   computed: {
     rows () {
-      return this.filteredData.length
+      return this.apiData.length
     }
   },
   methods:{
-    getfilteredData: function () {
-      this.filteredData = this.apiData
-      let filteredDataBySearch = []
-      // then filter according to keyword, for now this only affects the name attribute of each data
+    getDataFromApi: function () {
       if (this.search !== '') {
-        filteredDataBySearch = this.filteredData.filter(obj => obj.firstName.toLowerCase().indexOf(this.search.toLowerCase()) >= 0)      
-        this.filteredData = filteredDataBySearch
+        fetch('http://localhost:51770/api/customer/' + this.search, { 
+              method: 'get' 
+            })
+            .then((response) => response.json())
+            .then((jsonData) => {
+              this.apiData = jsonData
+            })
       }
-    }
+      else {
+        fetch('http://localhost:51770/api/customer', { 
+              method: 'get' 
+            })
+            .then((response) => response.json())
+            .then((jsonData) => {
+              this.apiData = jsonData
+            })
+      }
+    } 
   },
   mounted: function() {
-    fetch('http://localhost:51770/api/customer', { 
-      method: 'get' 
-    })
-    .then((response) => response.json())
-    .then((jsonData) => {
-      this.apiData = jsonData
-    })
+    this.getDataFromApi()
   }
 }
 </script>
